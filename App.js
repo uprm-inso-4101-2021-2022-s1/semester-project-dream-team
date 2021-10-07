9a27900d320322feeed19f4f85b537329f1d8847
jbachour <jamiliette.bachour@upr.edu>
2021-10-06 20:57:55 -0400

    added lint files + double quote rule

--- App.js
+++ App.js
@@ -1,22 +1,44 @@
-import { StatusBar as ExpoStatusBar} from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, Platform, StatusBar} from 'react-native';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from "react-native";
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={{flex: 1, marginTop: StatusBar.currentHeight}}>
-        <View style={{ padding: 16, backgroundColor: "green"}}>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
           <Text>search</Text>
-       </View>
-        <View style={{ flex: 1, padding: 16, backgroundColor: "blue"}}>
+        </View>
+        <View style={styles.list}>
           <Text>list</Text>
         </View>
       </SafeAreaView>
-      <ExpoStatusBar style="auto"/>
+      <ExpoStatusBar style="auto" />
     </>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+
+  search: {
+    padding: 16,
+    backgroundColor: "green",
+  },
+
+  list: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "blue",
+  },
 });
