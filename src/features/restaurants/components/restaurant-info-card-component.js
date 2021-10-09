616d04fdbf445c424902f357fce0ebd728fbed38
jbachour <jamiliette.bachour@upr.edu>
2021-10-09 14:16:23 -0400

    app's theme: colors, size, index, fonts

--- src/features/restaurants/components/restaurant-info-card-component.js
+++ src/features/restaurants/components/restaurant-info-card-component.js
@@ -1,6 +1,20 @@
 import React from "react";
 import { Text, StyleSheet } from "react-native";
 import { Card } from "react-native-paper";
+import styled from "styled-components/native";
+
+const Title = styled(Text)`
+  padding: 16px;
+  color: ${(props) => props.theme.colors.ui.primary};
+`;
+
+const RestaurantCard = styled(Card)`
+  background-color: white;
+`;
+const RestaurantCardCover = styled(Card.Cover)`
+  padding: 20px;
+  backgroundcolor: white;
+`;
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Some rest.",
@@ -15,15 +29,9 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
   } = restaurant;
 
   return (
-    <Card elevation={5} style={styles.card}>
-      <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
-      <Text style={styles.title}> {name} </Text>
-    </Card>
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+      <Title> {name} </Title>
+    </RestaurantCard>
   );
 };
-
-const styles = StyleSheet.create({
-  card: { backgroundColor: "white" },
-  cover: { padding: 20, backgroundColor: "white" },
-  title: { padding: 16 },
-});
