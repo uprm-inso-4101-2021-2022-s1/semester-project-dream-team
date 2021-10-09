7f5f831e5479d88b75ea3c6f7bd672810dc5da7b
jbachour <jamiliette.bachour@upr.edu>
2021-10-09 14:50:57 -0400

    changed styling to use our custom theme

--- src/features/restaurants/components/restaurant-info-card-component.js
+++ src/features/restaurants/components/restaurant-info-card-component.js
@@ -4,16 +4,16 @@ import { Card } from "react-native-paper";
 import styled from "styled-components/native";
 
 const Title = styled(Text)`
-  padding: 16px;
+  padding: ${(props) => props.theme.space[3]};
   color: ${(props) => props.theme.colors.ui.primary};
 `;
 
 const RestaurantCard = styled(Card)`
-  background-color: white;
+  background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 const RestaurantCardCover = styled(Card.Cover)`
-  padding: 20px;
-  backgroundcolor: white;
+  padding: ${(props) => props.theme.space[3]};
+  backgroundcolor: ${(props) => props.theme.colors.bg.primary};
 `;
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -18,11 +18,11 @@ const SafeArea = styled(SafeAreaView)`
   ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
 `;
 const SearchContainer = styled.View`
-  padding: 16px;
+  padding: ${(props) => props.theme.space[3]};
 `;
 const RestauranListContainer = styled.View`
   flex: 1;
-  padding: 16px;
+  padding: ${(props) => props.theme.space[3]};
 `;
 export const RestaurantsScreen = () => (
   <SafeArea>
