41f76a7c20e8a8d02598ad60be8dd07fea7b10ad
jbachour <jamiliette.bachour@upr.edu>
2021-11-01 17:18:59 -0400

    changed spacing

--- src/components/spacer/spacer.component.js
+++ src/components/spacer/spacer.component.js
@@ -3,27 +3,27 @@ import { View } from "react-native";
 import styled from "styled-components/native";
 
 const TopSmall = styled.View`
-  margin-top: ${(props) => props.theme.space[2]};
+  margin-top: ${(props) => props.theme.space[1]};
 `;
 
 const TopMedium = styled.View`
-  margin-top: ${(props) => props.theme.space[3]};
+  margin-top: ${(props) => props.theme.space[2]};
 `;
 
 const TopLarge = styled.View`
-  margin-top: ${(props) => props.theme.space[4]};
+  margin-top: ${(props) => props.theme.space[3]};
 `;
 
 const LeftSmall = styled.View`
-  margin-left: ${(props) => props.theme.space[2]};
+  margin-left: ${(props) => props.theme.space[1]};
 `;
 
 const LeftMedium = styled.View`
-  margin-left: ${(props) => props.theme.space[3]};
+  margin-left: ${(props) => props.theme.space[2]};
 `;
 
 const LeftLarge = styled.View`
-  margin-left: ${(props) => props.theme.space[4]};
+  margin-left: ${(props) => props.theme.space[3]};
 `;
 export const Spacer = ({ variant }) => {
   if (variant === "top.medium") {
