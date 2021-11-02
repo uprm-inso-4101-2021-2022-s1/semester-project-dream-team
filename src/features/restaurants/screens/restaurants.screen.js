91f75a28eff042664545753a0baa073d9f5c55e5
jbachour <jamiliette.bachour@upr.edu>
2021-11-01 22:43:09 -0400

    implemented bottom nav bar

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,13 +1,10 @@
 import React from "react";
 import { Searchbar } from "react-native-paper";
 import { RestaurantInfoCard } from "../components/restaurant-info-card-component";
-import { FlatList, SafeAreaView, StatusBar } from "react-native";
+import { FlatList } from "react-native";
 import styled from "styled-components/native";
 import { Spacer } from "../../../components/spacer/spacer.component";
-const SafeArea = styled(SafeAreaView)`
-  flex: 1;
-  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
-`;
+import { SafeArea } from "../../../components/utility/safe-area.components";
 const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `;
