d7b346bf4001dae42e475a559eee2854c197228a
jbachour <jamiliette.bachour@upr.edu>
2021-11-02 16:36:28 -0400

    created screens for different pages

---
+++ src/features/map/screens/map.screen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text } from "react-native-paper";
+import { SafeArea } from "../../../components/utility/safe-area.components";
+
+export const MapScreen = () => {
+  return (
+    <SafeArea>
+      <Text> Map </Text>
+    </SafeArea>
+  );
+};
