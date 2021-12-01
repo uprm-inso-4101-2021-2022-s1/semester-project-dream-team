cfab76356e896fd2b2fe6eeef89c0a03fa8c9934
jbachour <jamiliette.bachour@upr.edu>
2021-12-01 02:49:02 -0400

    front-end: account screen

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -1,11 +1,31 @@
 import React from "react";
+import { List } from "react-native-paper";
 import { Text } from "react-native-paper";
-import { SafeArea } from "../../../components/utility/safe-area.components";
-
-export const AccountScreen = () => {
+import { SafeArea } from "../../../components/utility/safe-area.component";
+import { Avatar } from "react-native-elements";
+export const AccountScreen = ({ navigation }) => {
   return (
     <SafeArea>
-      <Text> This is the Account Screen </Text>
+        <Avatar
+          rounded
+          size="large"
+          icon={{ name: "user", type: "font-awesome", color: "green" }}
+        />
+        <Text>Username</Text>
+        <Text>Level 21</Text>
+    
+      <List.Section>
+        <List.Item
+          style={{ padding: 16 }}
+          title="Achievements"
+          left={(props) => <List.Icon {...props} color="black" icon="trophy" />}
+        />
+        <List.Item
+          style={{ padding: 16 }}
+          title="Logout"
+          left={(props) => <List.Icon {...props} color="black" icon="door" />}
+        />
+      </List.Section>
     </SafeArea>
   );
 };
