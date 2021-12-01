7205dba39764e0c8a0a9e1e4fe4f2a059e306b0e
jbachour <jamiliette.bachour@upr.edu>
2021-12-01 02:48:51 -0400

    front-end: settings screen

--- src/features/settings/screens/settings.screen.js
+++ src/features/settings/screens/settings.screen.js
@@ -1,11 +1,24 @@
 import React from "react";
+import { List } from "react-native-paper";
 import { Text } from "react-native-paper";
-import { SafeArea } from "../../../components/utility/safe-area.components";
-
-export const SettingsScreen = () => {
+import { SafeArea } from "../../../components/utility/safe-area.component";
+export const SettingsScreen = ({ navigation }) => {
   return (
     <SafeArea>
-      <Text> This is the settings page</Text>
+      <List.Section>
+        <List.Item
+          style={{ padding: 16 }}
+          title="Favourites"
+          description="View your favourites"
+          left={(props) => <List.Icon {...props} color="black" icon="heart" />}
+          onPress={() => navigation.navigate("Favourites")}
+        />
+        <List.Item
+          style={{ padding: 16 }}
+          title="Logout"
+          left={(props) => <List.Icon {...props} color="black" icon="door" />}
+        />
+      </List.Section>
     </SafeArea>
   );
 };
--- src/infrastructure/navigation/app.navigator.js
+++ src/infrastructure/navigation/app.navigator.js
@@ -6,7 +6,8 @@ import { Text } from "react-native";
 import { SafeArea } from "../../components/utility/safe-area.component";
 import { RestaurantsNavigator } from "./restaurants.navigator";
 import { MapScreen } from "../../features/map/screens/map.screen";
-
+import { SettingsScreen } from "../../features/settings/screens/settings.screen";
+import { AccountScreen } from "../../features/account/screens/account.screen";
 const Tab = createBottomTabNavigator();
 const TAB_ICON = {
   Attractions: "md-paper-plane",
@@ -14,18 +15,6 @@ const TAB_ICON = {
   Account: "md-person",
   Settings: "md-settings",
 };
-
-const Settings = () => (
-  <SafeArea>
-    <Text>Settings</Text>
-  </SafeArea>
-);
-
-const Account = () => (
-  <SafeArea>
-    <Text>Account</Text>
-  </SafeArea>
-);
 const createScreenOptions = ({ route }) => {
   const iconName = TAB_ICON[route.name];
 
@@ -48,8 +37,8 @@ export const AppNavigator = () => (
     >
       <Tab.Screen name="Attractions" component={RestaurantsNavigator} />
       <Tab.Screen name="Map" component={MapScreen} />
-      <Tab.Screen name="Account" component={Account} />
-      <Tab.Screen name="Settings" component={Settings} />
+      <Tab.Screen name="Account" component={AccountScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
     </Tab.Navigator>
   </NavigationContainer>
 );
